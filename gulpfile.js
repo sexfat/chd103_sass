a67c82ae8890859e3a4e32a501544d2ee215c7bd
sexfat <bryant6831@gmail.com>
2023-08-18 13:43:02 +0800

    add sync

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,26 @@ const {
     watch
 } = require('gulp');
 
-//任務
+//任務1
 function defaultTask(cb) {
     console.log('hello 中午吃飯');
     cb();
 }
 
 exports.log = defaultTask;
+
+
+// 任務A
+function taskA(cb){
+    console.log('任務A');
+    cb();
+}
+
+// 任務B
+function taskB(cb){
+    console.log('任務B');
+    cb();
+}
+
+exports.sync = parallel(taskA , taskB);//同時執行A B任務
+exports.async = series(taskA , taskB);//執行A後再執行B任務
